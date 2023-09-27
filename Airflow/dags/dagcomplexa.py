68048ed967e93d03a255b303b8bb4563feac7572
Gabriel Sao Marcos <saomarcostecnologia@gmail.com>
2023-09-27 15:04:44 -0300

    subindo aulas

---
+++ Airflow/dags/dagcomplexa.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+dag = DAG('trigger3_dag', description='Nossa trigger3 DAG',
+        schedule_interval=None, start_date=datetime(2023,3,5), catchup=False)
+
+task1 = BashOperator(task_id="tsk1", bash_command="exit 1", dag=dag)
+task2 = BashOperator(task_id="tsk2", bash_command="exit 1", dag=dag)
+task3 = BashOperator(task_id="tsk3", bash_command="exit 1", dag=dag)
+task4 = BashOperator(task_id="tsk2", bash_command="exit 1", dag=dag)
+task5 = BashOperator(task_id="tsk2", bash_command="exit 1", dag=dag)
+task6 = BashOperator(task_id="tsk2", bash_command="exit 1", dag=dag)
+task7 = BashOperator(task_id="tsk2", bash_command="exit 1", dag=dag)
+task8 = BashOperator(task_id="tsk2", bash_command="exit 1", dag=dag)
+task9 = BashOperator(task_id="tsk2", bash_command="exit 1", dag=dag,
+                    trigger_rule = 'all_failed')
+
+task1 >> task2
+task3 >> task4
+[]
+[task1,task2] >> task3
+
